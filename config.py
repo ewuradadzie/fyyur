9068a49d9966ff7bbd37d08d78c8b0afa0f136a2
Ewuradjoa Dadzie <ewura.dadzie@gmail.com>
2022-06-04 23:54:32 +0000

    create and read

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import psycopg2
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,6 +10,10 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+dbuser = 'postgres'
+dbpass = 'postgres'
+dbhost = 'localhost'
+dbport = 5432 
+dbname = 'fyyur'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}'
